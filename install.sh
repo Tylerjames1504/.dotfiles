068a95716b1af1b6c8610e50ef4ca3e582295ad0
Tylerjames1504 <63423120+Tylerjames1504@users.noreply.github.com>
2024-01-06 23:14:38 -0500

    makes sure that stow is installed

--- install.sh
+++ install.sh
@@ -4,6 +4,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DOT_FOLDERS="nvim"
 
+
+# Check if the operating system is Ubuntu
+if [[ "$(uname)" == "Linux" && -e "/etc/lsb-release" ]]; then
+    # Ubuntu-specific command
+    sudo apt install stow
+# Check if the operating system is macOS
+elif [[ "$(uname)" == "Darwin" ]]; then
+    # macOS-specific command
+    brew install stow
+else
+    # Display an error message for unsupported OS
+    echo "Unsupported operating system"
+    exit 1
+fi
+
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
     stow --ignore=README.md --ignore=LICENSE \
